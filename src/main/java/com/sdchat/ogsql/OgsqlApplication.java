28be7be2a038e2e24e47b4098420b6a8e5906531
Chen Jianjun <chenjj.yz@gmail.com>
2026-01-12 23:41:53 +0800

    Initial commit from Specify template

---
+++ src/main/java/com/sdchat/ogsql/OgsqlApplication.java
@@ -0,0 +1,13 @@
+package com.sdchat.ogsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgsqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdchat/ogsql/OgsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdchat.ogsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
